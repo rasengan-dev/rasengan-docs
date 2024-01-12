b75a63ee6b72b884b9cc1a6ad1310af46e90bc77
dilane3 <komboudilane125@gmail.com>
2024-01-12 18:39:49 +0100

    Changing all primary configuration about the owner of the project and some others elements

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Rasengan</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/rasengan-dev/rasenganjs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/rasengan-dev/rasenganjs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Rasenganjs © 2024',
   },
 }
 
