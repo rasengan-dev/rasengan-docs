f850159a9a78d24bc145f01ec4d8c66a3943c492
dilane3 <komboudilane125@gmail.com>
2024-02-07 23:14:48 +0100

    Setting up the color shade into tailwind config file and creating a Custom Alert Component

---
+++ components/atoms/Alert.tsx
@@ -0,0 +1,117 @@
+import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+import { useTheme } from "nextra-theme-docs";
+import Icon from "@components/atoms/Icon";
+
+type Props = {
+  children: React.ReactNode;
+  type?: "success" | "error" | "info" | "tip";
+};
+
+export default function AlertBox({ children, type }: Props) {
+  const { resolvedTheme: theme } = useTheme();
+
+  // Memoization
+  const { bgColorName, iconColorName, borderColorName } = useMemo(() => {
+    let bgColorName = "";
+    let iconColorName = "";
+    let borderColorName = "";
+    const textColorName = theme === "dark" ? "text-white" : "text-black";
+
+    switch (type) {
+      case "success": {
+        bgColorName =
+          theme === "dark" ? "bg-alert-success-dark" : "bg-alert-success-light";
+        iconColorName = "text-alert-success";
+        borderColorName = "border-alert-success";
+
+        return {
+          bgColorName,
+          iconColorName,
+          borderColorName,
+        };
+      }
+
+      case "error": {
+        bgColorName =
+          theme === "dark" ? "bg-alert-error-dark" : "bg-alert-error-light";
+        iconColorName = "text-alert-error";
+        borderColorName = "border-alert-error";
+
+        return {
+          bgColorName,
+          iconColorName,
+          borderColorName,
+        };
+      }
+
+      case "info": {
+        bgColorName =
+          theme === "dark" ? "bg-alert-info-dark" : "bg-alert-info-light";
+        iconColorName = "text-alert-info";
+        borderColorName = "border-alert-info";
+
+        return {
+          bgColorName,
+          iconColorName,
+          borderColorName,
+        };
+      }
+
+      case "tip": {
+        bgColorName =
+          theme === "dark" ? "bg-alert-warning-dark" : "bg-alert-warning-light";
+        iconColorName = "text-alert-warning";
+        borderColorName = "border-alert-warning";
+
+        return {
+          bgColorName,
+          iconColorName,
+          borderColorName,
+        };
+      }
+
+      default: {
+        bgColorName =
+          theme === "dark" ? "bg-alert-info-dark" : "bg-alert-info-light";
+        iconColorName = "text-alert-info";
+        borderColorName = "border-alert-info";
+
+        return {
+          bgColorName,
+          iconColorName,
+          borderColorName,
+        };
+      }
+    }
+  }, [type, theme]);
+
+  const iconName = useMemo(() => {
+    switch (type) {
+      case "success":
+        return "check-circle-fill";
+      case "error":
+        return "x-circle-fill";
+      case "info":
+        return "info-circle-fill";
+      case "tip":
+        return "lightbulb-fill";
+      default:
+        return "info-circle-fill";
+    }
+  }, [type]);
+
+
+  return (
+    <div
+      className={twMerge(
+        "w-full rounded-lg border-2 px-4 py-2 flex overflow-hidden",
+        `${bgColorName} ${borderColorName}`
+      )}
+    >
+      <Icon name={iconName} size={24} className={twMerge("mr-4", iconColorName)} />
+
+      <div>{children}</div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,59 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#2A7FFF",
+        primary: {
+          DEFAULT: "#2A7FFF",
+          100: "#EAF2FF",
+          200: "#D4E5FF",
+          300: "#BFD9FF",
+          400: "#AACCFF",
+          500: "#5599FF",
+          600: "#2266CC",
+          700: "#113366",
+          800: "#0D264D",
+          900: "#081933",
+        },
+
+        secondary: {
+          DEFAULT: "#6F70EB",
+          100: "#E2E2FB",
+          200: "#D4D4F9",
+          300: "#C5C6F7",
+          400: "#8C8DEF",
+          500: "#5A5BC5",
+          600: "#45469F",
+          700: "#303078",
+          800: "#262665",
+          900: "#1B1B52",
+        },
+
+        alert: {
+          info: {
+            DEFAULT: "#6BA4FA",
+            light: "#f2f6fc",
+            dark: "#1d2529"
+          },
+          success: {
+            DEFAULT: "#0F9D58",
+            light: "#f2f6fc",
+            dark: "#1d2529"
+          },
+          warning: {
+            DEFAULT: "#F2B200",
+            light: "#f2f6fc",
+            dark: "#1d2529"
+          },
+          error: {
+            DEFAULT: "#FF6666",
+            light: "#f2f6fc",
+            dark: "#1d2529"
+          },
+        },
+
+        gray: {
+          DEFAULT: "#AAA7AD",
+          500: "#AAAAAA"
+        }
       },
 
       fontFamily: {
