3b33ce387b0a44fc8a8ef2506cf5493d2141dd33
dilane3 <komboudilane125@gmail.com>
2024-03-20 19:22:30 +0100

    update: changing /docs/api to /docs/api-reference

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -4,8 +4,18 @@ import sleep from "utils/sleep";
 
 export default function useTheme() {
   // Using theme
-  const { resolvedTheme: theme, theme: baseTheme, ...rest } = useDefaultTheme();
+  const {
+    resolvedTheme: theme,
+    theme: baseTheme,
+    setTheme,
+    ...rest
+  } = useDefaultTheme();
   const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    if (theme === "dark" || theme === "light") setTheme("system");
+    else setTheme("light");
+  }, []);
 
   const isDark = useMemo(() => {
     return theme === "dark";
@@ -22,7 +32,7 @@ export default function useTheme() {
   return {
     isDark,
     theme: baseTheme,
-    setTheme: rest.setTheme,
+    setTheme,
     themeList: rest.themes,
   };
 }
